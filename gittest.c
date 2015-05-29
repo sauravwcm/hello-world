aabe6168ea330b9e01677b1ddb9094d2bbd5c9ab
sauravwcm <paulatcollege10@gmail.com>
2015-05-29 12:39:48 +0200

    added a loop in the code

--- gittest.c
+++ gittest.c
@@ -3,6 +3,12 @@
 
 int main(int argc, char const *argv[])
 {
+	unsigned char i;
 	printf("this is a git test c file\n");
+
+	for (i = 0; i < 100; ++i)
+	{
+		printf("printing index value: %c\n",i );
+	}
 	return 0;
 }
